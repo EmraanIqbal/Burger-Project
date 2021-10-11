b82050a0fef5595b0a6d6940484de9d7162269de
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-11 05:00:29 -0700

    Orders.js

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,21 +9,6 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
   componentDidMount() {
-    // axios
-    //   .get("/orders.json")
-    //   .then((res) => {
-    //     const fetchOrders = [];
-    //     for (let key in res.data) {
-    //       fetchOrders.push({
-    //         ...res.data[key],
-    //         id: key,
-    //       });
-    //     }
-    //     this.setState({ loading: false, orders: fetchOrders });
-    //   })
-    //   .catch((err) => {
-    //     this.setState({ loading: false });
-    //   });
     this.props.onFetchOrders(this.props.token, this.props.userId);
   }
   render() {
