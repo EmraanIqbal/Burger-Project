acdb9464bb38614ab8281cdaabef53f817de3932
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-11 05:05:49 -0700

    container/Auth/Auth.js

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -50,25 +50,6 @@ class Auth extends Component {
     }
   }
 
-  // checkValidity(value, rules) {
-  //   let isValid = true;
-
-  //   if (!rules) {
-  //     return true;
-  //   }
-
-  //   if (rules.required) {
-  //     isValid = value.trim() !== "" && isValid;
-  //   }
-  //   if (rules.minLength) {
-  //     isValid = value.length >= rules.minLength && isValid;
-  //   }
-  //   if (rules.maxLength) {
-  //     isValid = value.length <= rules.maxLength && isValid;
-  //   }
-  //   return isValid;
-  // }
-
   inputChangedHandler = (event, controlName) => {
     const updatedControls = updateObject(this.state.controls, {
       [controlName]: updateObject(this.state.controls[controlName], {
